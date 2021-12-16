994b86c27af066df4da04d6b35083a3685a69523
Tim Honchel <tim.honchel@gmail.com>
2021-12-16 11:01:58 -0500

    first commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a temperature in Fahrehenheit:");
+            double f = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The temperature in Celsius is {TempConverter.FahrenheitToCelsius(f)}");
+            Console.WriteLine();
+            Console.WriteLine("Enter a temperature in Celsius:");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The temperature in Fahrenheit is {TempConverter.CelsiusToFahrenheit(c)}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f)
+        {
+            return (f-32)*5/9;
+        }
+
+        public static double CelsiusToFahrenheit(double c)
+        {
+            return c*9/5+32;
+        }
+
+    }
+}
